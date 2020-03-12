6bc36983ef762304affaaf02123350d3f346a737
GBTC440008UR <GBTC440008UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-03-11 20:08:22 -0400

    Initial commit

---
+++ src/test/java/com/springboot204/springboot_204/Springboot204ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot204.springboot_204;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot204ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
